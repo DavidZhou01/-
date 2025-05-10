cab613961683f26fa4eb04fb0b102dda1664ad5f
DavidZhou01 <1519306644@qq.com>
2025-05-10 15:27:01 +0800

    根据id查询员工与修改员工信息代码开发

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -107,12 +107,31 @@ public class EmployeeController {
      * 启用/禁用员工账号
      * @param status
      * @param id
-     * @return
      */
     @PostMapping("/status/{status}")
     @ApiOperation("员工状态禁用/启用")
     public Result startOrStop(@PathVariable Integer status, Long id){
         employeeService.startOrStop(status, id);
+        return Result.success();
+    }
+
+    /**
+     * 根据id查询员工信息
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    @ApiOperation("根据id查询员工")
+    public Result<Employee> getById(@PathVariable Long id){
+        Employee employee = employeeService.getById(id);
+        return Result.success(employee);
+    }
+
+    @PutMapping
+    @ApiOperation("修改员工信息")
+    public Result update(@RequestBody EmployeeDTO employeeDTO){
+        log.info("员工修改信息：{}", employeeDTO);
+        employeeService.update(employeeDTO);
         return Result.success();
     }
 }
